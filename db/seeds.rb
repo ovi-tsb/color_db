9584864db4cc805e70324887e4055d4f82681669
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-05-26 20:29:01 -0400

    Re setup db on heroku with user migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Customer.create(name: 'Ellis Gueplh')
-# Customer.create(name: 'Royal Anvelope')
-# Customer.create(name: 'Ellis Mississauga')
+Customer.create(name: 'Ellis Gueplh')
+Customer.create(name: 'Royal Anvelope')
+Customer.create(name: 'Ellis Mississauga')
 
-# User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Tom", last_name: "Davis")
+User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Tom", last_name: "Davis")
 SuperUser.create!(email: "super@test.com", password: "123456", password_confirmation: "123456", first_name: "Brad", last_name: "Pit", title: "top")
 
 #####  CSV Ellis Mississauga
-# require 'csv'
+require 'csv'
 
-# CSV.foreach("lib/seeds/EllisPaperBoxDrawdownIndexCSV2.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-#   Ink.create(row.to_hash)
-# end
+CSV.foreach("lib/seeds/EllisPaperBoxDrawdownIndexCSV2.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Ink.create(row.to_hash)
+end
